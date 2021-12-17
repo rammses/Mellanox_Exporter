976355e799b9c6440115ac3a410414f8d5d6ce4b
rammses <Zhs42fmV42>
2021-12-17 21:09:41 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+
+def get_directories(path):
+    list_of_directories  = None
+    return list_of_directories
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
